45e7984594c713696be29111835fc4b3fd82890a
cristinam <mateescu.cristina24@yahoo.ro>
2019-06-10 16:12:49 +0300

    initial commit

---
+++ src/ConditionalOperator.java
@@ -0,0 +1,7 @@
+public class ConditionalOperator {
+    public static void main(String[] args) {
+        int a=18;
+        String s= a>18 ? "ok": "Not OK";
+        System.out.println(s);
+    }
+}
